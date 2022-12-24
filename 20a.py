60f2c0184d12e67bc35ff797988da6c6b444b39d
Prakul-Agrawal <prakulagrawal.2003@gmail.com>
2022-12-24 17:13:47 +0530

    Day 20

---
+++ 20a.py
@@ -0,0 +1,26 @@
+l = []
+while True:
+    s = int(input())
+    if s == 999999:
+        break
+    l.append(s)
+
+array = list(enumerate(l))
+
+length = len(array)
+
+for i in range(length):
+    for j in range(length):
+        if array[j][0] == i:
+            val = array.pop(j)
+            pos = (val[1] + j) % (length - 1)
+            if pos == 0:
+                array.append(val)
+            else:
+                array.insert(pos, val)
+            break
+
+for i in range(length):
+    if array[i][1] == 0:
+        print(sum(array[(i + j) % length][1] for j in (1000,2000,3000)))
+        break
